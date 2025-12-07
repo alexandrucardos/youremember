6700fd56ffb93bb04d2e58ee7ec05f49baf3002a
Alexandru Cardos <alexandru.cardos@emag.ro>
2025-12-07 12:21:45 +0200

    [HEAD] added twig doctrine security and profiler

---
+++ docker/php-fpm/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$ENVIRONMENT" == "docker" ]
+then
+    APP_ENV=$APP_ENV composer install --prefer-dist --no-progress --no-suggest --no-interaction
+    COMPOSER_ALLOW_SUPERUSER=1 composer run-script install-git-hook
+
+    chmod -R 777 /var/www/heyblu/var
+else
+    cp ./docker/prod/php-fpm/docker-php-ext-amqp.ini /usr/local/etc/php/conf.d/
+    cp ./docker/prod/php-fpm/root_ca.pem /var/www/html/root_ca.pem
+fi
+
+if [ -z "$WORKERS_ENABLED" ]; then
+  php-fpm
+else
+  /usr/bin/supervisord -c /etc/supervisord.conf
+  supervisorctl start all
+  tail -f /dev/null
+fi
