e00af0819f5329718f53bac9bff79f7e8a3d8b00
Alexandru Cardos <alexandru.cardos@emag.ro>
2025-12-09 14:45:22 +0200

    added CRUD for profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Profile;
+use App\Form\ProfileType;
+use App\Repository\ProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/profile')]
+final class ProfileController extends AbstractController
+{
+    #[Route(name: 'app_profile_index', methods: ['GET'])]
+    public function index(ProfileRepository $profileRepository): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'profiles' => $profileRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_profile_show', methods: ['GET'])]
+    public function show(Profile $profile): Response
+    {
+        return $this->render('profile/show.html.twig', [
+            'profile' => $profile,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Profile $profile, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'profile' => $profile,
+            'form' => $form,
+        ]);
+    }
+}
