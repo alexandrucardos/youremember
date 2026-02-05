937545415c112959252ab22b176db699bba76bba
Alexandru Cardos <alexandru.cardos@emag.ro>
2026-02-05 13:50:00 +0200

    [authchanges] added media entity

---
+++ src/Entity/Media.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MediaRepository::class)]
+class Media
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'media')]
+    #[ORM\JoinColumn(nullable: false)]
+    private Event $event;
+
+    #[ORM\Column(length: 500)]
+    private string $file_path;
+
+    #[ORM\Column(length: 500)]
+    private string $thumbnail_path;
+
+    #[ORM\Column(length: 50)]
+    private string $uploader_hash;
+
+    #[ORM\Column(length: 20)]
+    private string $file_type;
+
+    #[ORM\Column]
+    private int $file_size;
+
+    #[ORM\Column(length: 255)]
+    private string $original_filename;
+
+    #[ORM\Column(
+        insertable: false,
+        updatable: false,
+        options: ['default' => 'CURRENT_TIMESTAMP']
+    )]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(
+        insertable: false,
+        updatable: false,
+        options: [
+            'default' => 'CURRENT_TIMESTAMP',
+            'on update' => 'CURRENT_TIMESTAMP'
+        ])]
+    private ?\DateTimeImmutable $modified_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEvent(): Event
+    {
+        return $this->event;
+    }
+
+    public function setEvent(Event $event): static
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    public function getFilePath(): string
+    {
+        return $this->file_path;
+    }
+
+    public function setFilePath(string $file_path): static
+    {
+        $this->file_path = $file_path;
+
+        return $this;
+    }
+
+    public function getThumbnailPath(): ?string
+    {
+        return $this->thumbnail_path;
+    }
+
+    public function setThumbnailPath(string $thumbnail_path): static
+    {
+        $this->thumbnail_path = $thumbnail_path;
+
+        return $this;
+    }
+
+    public function getUploaderHash(): string
+    {
+        return $this->uploader_hash;
+    }
+
+    public function setUploaderHash(string $uploader_hash): static
+    {
+        $this->uploader_hash = $uploader_hash;
+
+        return $this;
+    }
+
+    public function getFileType(): ?string
+    {
+        return $this->file_type;
+    }
+
+    public function setFileType(string $file_type): static
+    {
+        $this->file_type = $file_type;
+
+        return $this;
+    }
+
+    public function getFileSize(): ?int
+    {
+        return $this->file_size;
+    }
+
+    public function setFileSize(int $file_size): static
+    {
+        $this->file_size = $file_size;
+
+        return $this;
+    }
+
+    public function getOriginalFilename(): ?string
+    {
+        return $this->original_filename;
+    }
+
+    public function setOriginalFilename(?string $original_filename): static
+    {
+        $this->original_filename = $original_filename;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): \DateTimeImmutable
+    {
+        return $this->modified_at;
+    }
+
+    public function setModifiedAt(\DateTimeImmutable $modified_at): static
+    {
+        $this->modified_at = $modified_at;
+
+        return $this;
+    }
+}
---
+++ src/Repository/MediaRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Media;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Media>
+ */
+class MediaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Media::class);
+    }
+
+    public function save(Media $media): void
+    {
+        $this->getEntityManager()->persist($media);
+
+        $this->getEntityManager()->flush();
+    }
+}
