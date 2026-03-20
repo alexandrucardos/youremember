003c21e70e84c004b21f85795c04be1e6c681734
Alexandru Cardos <alexandru.cardos@emag.ro>
2026-03-20 09:19:17 +0200

    mago

--- src/Domain/Model/Profile/ProfileEntity.php
+++ src/Domain/Model/Profile/ProfileEntity.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Domain\Model\Profile;
 
@@ -51,8 +51,7 @@ class ProfileEntity
 
     public function __construct(
         public readonly UuidValueObject $profileUuidValueObject
-    )
-    {
+    ) {
     }
 
     public function addMediaPathsForDeletion(
@@ -80,16 +79,15 @@ class ProfileEntity
 
     public function setMediaFiles(
         array $files,
-        int   $maxItems,
-        int   $existingItems,
+        int $maxItems,
+        int $existingItems,
         array $uniqueMimeTypes
-    ): self
-    {
+    ): self {
         if (empty($files)) {
             throw new MissingFiles('Missing files from request');
         }
 
-        if ($maxItems < ($existingItems + count($files))) {
+        if ($maxItems < ( $existingItems + count($files) )) {
             throw new MaximumProfileItemsReachedException('Maximum number of files reached!');
         }
 
