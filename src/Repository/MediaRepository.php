74f5ac739595d9d4d5cb1465d8180b7934b6c419
Alexandru Cardos <alexandru.cardos@emag.ro>
2026-03-25 09:29:48 +0200

    cleanup

--- src/Repository/MediaRepository.php
+++ src/Repository/MediaRepository.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Repository;
 
-use App\Domain\Model\Profile\MediaRepositoryInterface;
 use App\Entity\Media;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
