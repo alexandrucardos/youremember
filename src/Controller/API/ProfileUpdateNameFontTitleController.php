7a838501f358dea96ec0023e21234b3c1766df69
Alexandru Cardos <alexandru.cardos@emag.ro>
2026-06-26 11:03:40 +0300

    [in-memory-of] fix for new route

--- src/Controller/API/ProfileUpdateNameFontTitleController.php
+++ src/Controller/API/ProfileUpdateNameFontTitleController.php
@@ -21,14 +21,15 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 #[Route('/api/v1/update/name/font/title')]
 final class ProfileUpdateNameFontTitleController extends AbstractController
 {
-    public const NAME_PROFILE_NAME_UPDATE = 'api_profile_name_update';
+    public const NAME_PROFILE_NAME_UPDATE = 'api_profile_name_title_update';
 
     #[Route('/orderId/{order_id}', name: self::NAME_PROFILE_NAME_UPDATE, methods: ['PATCH'])]
     public function update(
-        Request $request,
+        Request                          $request,
         UpdateProfileNameAndTitleHandler $profileNameHandler,
-        TranslatorInterface $translator
-    ): JsonResponse {
+        TranslatorInterface              $translator
+    ): JsonResponse
+    {
         $userRole = $request->attributes->get(SecurityValidationRequestSubscriber::REQUEST_ATTRIBUTE_USER_ROLE);
 
         if (!in_array($userRole, UserRole::getAdminRoles(), true)) {
